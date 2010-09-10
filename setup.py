bac5766bd96611d76962dfdd12ea68da2de8fbc6
Brandon R. Stoner <monokrome@monokro.me>
2010-09-10 09:57:06 -0600

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='django-aliases',
+    version='0.1',
+    description='Some useful tools for "aliasing" objects in django.',
+    author='Brandon R. Stoner',
+    author_email='monokrome@monokro.me',
+    url='http://monokro.me/',
+    download_url='http://github.com/monokrome/django-aliases/',
+    license='MIT',
+    packages=['aliases'],
+)
+
