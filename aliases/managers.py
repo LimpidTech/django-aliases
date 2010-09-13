2b72537eafb971b09fc78cf84e2a41e5b6198492
Brandon R. Stoner <monokrome@monokro.me>
2010-09-13 10:07:56 -0600

    Let's see what this does...

--- aliases/managers.py
+++ aliases/managers.py
@@ -1,9 +1,8 @@
 from django.db.models import Manager
 from django.contrib import contenttypes
-import models
 
 class URLManager(Manager):
     def related_to(self, obj):
-        content_type = contenttypes.models.ContentType.objects.get_for_model(models.URL)
+        content_type = contenttypes.models.ContentType.objects.get_for_model(obj.model_class())
         return super(URLManager, self).get_query_set().filter(content_type=content_type, object_id=obj.pk)
 
