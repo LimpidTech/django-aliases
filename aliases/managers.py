e805aa16216c2c96da99c24461d60a41a577171a
Brandon R. Stoner <monokrome@monokro.me>
2010-09-13 10:40:55 -0600

    I need to wake up... Then this would work.

--- aliases/managers.py
+++ aliases/managers.py
@@ -3,6 +3,6 @@ from django.contrib import contenttypes
 
 class URLManager(Manager):
     def related_to(self, obj):
-        content_type = contenttypes.models.ContentType.objects.get_for_model(obj.model_class())
+        content_type = contenttypes.models.ContentType.objects.get_for_model(obj.__class__)
         return super(URLManager, self).get_query_set().filter(content_type=content_type, object_id=obj.pk)
 
