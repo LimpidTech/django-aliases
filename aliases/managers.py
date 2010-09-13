2c3a3c7551b0d2e5503cfe38695a26d465645dd3
Brandon R. Stoner <monokrome@monokro.me>
2010-09-13 09:46:32 -0600

    Fixed a small issue.

--- aliases/managers.py
+++ aliases/managers.py
@@ -2,7 +2,7 @@ from django.db.models import Manager
 from django.contrib import contenttypes
 import models
 
-class URLManager(models.Manager):
+class URLManager(Manager):
     def related_to(self, obj):
         content_type = contenttypes.models.ContentType.get_object_for_this_type(models.URL)
         return super(URLManager, self).get_query_set().filter(content_type=content_type, object_id=obj.pk)
