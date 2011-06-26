be0da65b442f684fbd918f4d3e98730ab7f0d512
Brandon R. Stoner <monokrome@monokro.me>
2011-06-26 02:38:44 +0000

    Updated setup process

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import os
 
-setup(
-    name='django-aliases',
-    version='0.1',
-    description='Some useful tools for "aliasing" objects in django.',
-    author='Brandon R. Stoner',
-    author_email='monokrome@monokro.me',
-    url='http://monokro.me/',
-    download_url='http://github.com/monokrome/django-aliases/',
-    license='MIT',
-    packages=['aliases'],
+parent_directory = os.path.abspath(os.path.dirname(__file__))
+
+meta_files = {
+    'README.md': None,
+    'CLASSIFIERS.txt': None,
+}
+
+for filename in meta_files:
+    try:
+        current_file = open(os.path.join(parent_directory, filename))
+        meta_files[filename] = current_file.read()
+        current_file.close()
+    except IOError:
+        raise IOError('{0} not found.'.format(filename))
+
+classifiers = meta_files['CLASSIFIERS.txt'].split('\n')
+classifiers.remove('')
+
+setup(name='django-aliases',
+      version='0.1',
+      description='Some useful tools for "aliasing" objects in django.',
+      long_description=meta_files['README.md'],
+      classifiers=classifiers,
+      author='Brandon R. Stoner',
+      author_email='monokrome@limpidtech.com',
+      url='http://github.com/LimpidTech/django-aliases',
+      packages=['aliases'],
+      keywords = 'web django aliases shortcuts',
 )
 
